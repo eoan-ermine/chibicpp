3704e618e4d843c5243de47b392edb803c085610
Danil Sidoruk <danilsidoruknv@yandex.ru>
2024-10-09 21:51:26 +0300

    Add .clang-format

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,13 +1,13 @@
 #include <fmt/format.h>
 
 int main(int argc, char **argv) {
-	if (argc != 2) {
-		fmt::println("{}: invalid number of arugment", argv[0]);
-		return 1;
-	}
+  if (argc != 2) {
+    fmt::println("{}: invalid number of arugment", argv[0]);
+    return 1;
+  }
 
-	fmt::println("\t.globl main");
-	fmt::println("main:");
-	fmt::println("\tmov ${}, %rax", std::atoi(argv[1]));
-	fmt::println("\tret");
+  fmt::println("\t.globl main");
+  fmt::println("main:");
+  fmt::println("\tmov ${}, %rax", std::atoi(argv[1]));
+  fmt::println("\tret");
 }
